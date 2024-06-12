048c34f7beb3b461cbcc8519225d946254d8c6f9
Yugal D Dekate <yugaldekate72@gmail.com>
2024-06-12 21:01:58 +0530

    created the design page, uploaded the cropped image url and other material choices

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Toaster } from "@/components/ui/toaster";
+import Providers from "@/components/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,7 +23,9 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
 
                 <main className='flex flex-col min-h-[calc(100vh-3.5rem-1px)]'>
                     <div className='flex-1 flex flex-col h-full'>
-                        {children}
+                        <Providers>
+                            {children}
+                        </Providers>
                     </div>
                     <Footer/>
                 </main>
---
+++ src/components/Providers.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { ReactNode } from 'react';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
+const Providers = ({ children }: { children: ReactNode }) => {
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    );
+}
+
+export default Providers
